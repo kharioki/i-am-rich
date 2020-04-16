11243e395df9db1348ae631de1b61cd9a863560d
kharioki <antolisah@gmail.com>
2020-04-17 02:18:05 +0300

    initial app setup

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// the main function is the starting point for all our flutter apps
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text('Hello world'),
+        ),
+      ),
+    );
